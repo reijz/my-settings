584baa8b1fa27c5cafebb2a4ca2ad97396669cc6
Jiheng Zhang <jiheng@ust.hk>
2015-02-17 22:47:08 +0800

    Delete selection and some key bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,14 @@
 (setq ispell-program-name "aspell")
 (add-to-list 'exec-path "/usr/local/bin")
 
+;; Selection delete mode
+(delete-selection-mode 1)
+
+;; some key bindings
+(global-set-key (kbd "<s-left>") 'beginning-of-line)
+(global-set-key (kbd "<s-right>") 'end-of-line)
+
+;; Add load path
 (add-to-list 'load-path "~/.emacs.d/jz-lisp")
 
 ;; Appearance of Emacs
