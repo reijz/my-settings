a2fe44ba2c1dbf36e44ec147726fde4ca6142616
Jiheng Zhang <jiheng@ust.hk>
2015-02-22 16:52:33 +0800

    Set redo key binding with redo+

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,29 @@
 ;; Starts the Emacs server
 (server-start)
 
-;; use aspell to spell checking
+;; Use aspell to spell checking
 (setq ispell-program-name "aspell")
 (add-to-list 'exec-path "/usr/local/bin")
 
 ;; Selection delete mode
 (delete-selection-mode 1)
 
-;; some key bindings
+;; Some key bindings
 (global-set-key (kbd "<s-left>") 'beginning-of-line)
 (global-set-key (kbd "<s-right>") 'end-of-line)
 
 ;; Add load path
 (add-to-list 'load-path "~/.emacs.d/jz-lisp")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; Appearance of Emacs
 (require 'jz-appearance)
 
 ;; Language settings
 (require 'jz-latex-mode)
+
+;; Redo using redo+.el
+;; from http://www.emacswiki.org/emacs/RedoPlus
+(require 'redo+)
+(global-set-key (kbd "<C-s-268632090>") 'redo)
+(global-set-key (kbd "C-?") 'redo)
